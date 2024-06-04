a223791307540b8f182a2d7842b939271af1760a
DR7262 <colina.rbm@gmail.com>
2024-06-04 20:54:24 +1000

    Removed flag system, added checks for caps chars & digits

--- main.py
+++ main.py
@@ -6,13 +6,16 @@
 # Revision History:
 # 2/1/2022 changed the name of the file variable
 
+import sys
+
 print("Password Checker V0.1")
 
 user_pw = input("Please enter your password:")
 
-flags = 0
+# 10 - 25 is the current acceptable password length
 if len(user_pw) not in range(10, 25):
-    flags+=1
+    print("Password is not strong enough - please revise.")
+    sys.exit()
 
 symbols_allowed = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-',
     '_', '=', '+', '[', '{', ']', '}', '|', ';', ':', ',', '<', '.', '>', '?']
@@ -20,13 +23,29 @@ symbols_allowed = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-',
 symbols_in_password = 0
 for character in user_pw:
     if character in symbols_allowed:
-        symbols_in_password +=1
-
-# As discussed, minimum number of acceptable symbols in password is 2
+        symbols_in_password += 1
+# 2 is currently the minimum number of acceptable symbols required in the password
 if symbols_in_password < 2:
-    flags+=1
-
-if flags == 0:
-    print("Password is strong enough.")
-else:
     print("Password is not strong enough - please revise.")
+    sys.exit()
+    
+
+numbers_in_password = 0
+for character in user_pw:
+    if character in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+        numbers_in_password += 1
+# 2 is currently the minimum number of digits required in the password
+if numbers_in_password < 2:
+    print("Password is not strong enough - please revise.")
+    sys.exit()
+
+uppercase_chars_in_password = 0
+for character in user_pw:
+    if character.isupper():
+        uppercase_chars_in_password += 1
+#2 is currently the minimum number of uppercase characters required in the password
+if uppercase_chars_in_password < 2:
+    print("Password is not strong enough - please revise.")
+    sys.exit()
+
+print("Password is strong enough.")
