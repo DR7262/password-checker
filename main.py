66bb0cec9d236595e6e0d47916fc0ff33c8f12c3
DR7262 <colina.rbm@gmail.com>
2024-06-04 20:33:47 +1000

    Updated condition checking method, added symbols to password requirements

--- main.py
+++ main.py
@@ -10,7 +10,23 @@ print("Password Checker V0.1")
 
 user_pw = input("Please enter your password:")
 
-if len(user_pw) in range(10, 25):
+flags = 0
+if len(user_pw) not in range(10, 25):
+    flags+=1
+
+symbols_allowed = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-',
+    '_', '=', '+', '[', '{', ']', '}', '|', ';', ':', ',', '<', '.', '>', '?']
+
+symbols_in_password = 0
+for character in user_pw:
+    if character in symbols_allowed:
+        symbols_in_password +=1
+
+# As discussed, minimum number of acceptable symbols in password is 2
+if symbols_in_password < 2:
+    flags+=1
+
+if flags == 0:
     print("Password is strong enough.")
 else:
     print("Password is not strong enough - please revise.")
